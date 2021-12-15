e81d879e3e438d4519018722d04aaf226fc689cd
Arnib Q <71690828+UchihaCryptic@users.noreply.github.com>
2021-12-15 14:42:25 -0800

    Fixed alphabet range for valid MAC address

--- mac_addr_generator.py
+++ mac_addr_generator.py
@@ -11,14 +11,17 @@ def generator():
                 continue
         except:
             print("Choose '1', '2', or '3'")
-    
+    alphabet = ['A','B','C','D','E','F']
     # Default MAC address Generation
     if input_value == 1:
         address = ""
-        int_or_char_list = [str(random.randint(0,9)), random.choice(string.ascii_uppercase)]
-        for x in range(5):
-            address = address + random.choice(int_or_char_list) + random.choice(int_or_char_list) + ":"
-        address = address + random.choice(int_or_char_list) + random.choice(int_or_char_list)
+        int_or_char_list1 = [str(random.randint(0,9)), random.choice(alphabet)]
+        int_or_char_list2 = [str(random.randint(0,9)), random.choice(alphabet)]
+        int_or_char_list3 = [str(random.randint(0,9)), random.choice(alphabet)]
+        int_or_char_list4 = [str(random.randint(0,9)), random.choice(alphabet)]
+        int_or_char_list5 = [str(random.randint(0,9)), random.choice(alphabet)]
+        int_or_char_list6 = [str(random.randint(0,9)), random.choice(alphabet)]
+        address = random.choice(int_or_char_list1) + random.choice(int_or_char_list1) + ":" + random.choice(int_or_char_list2) + random.choice(int_or_char_list2) + ":" + random.choice(int_or_char_list3) + random.choice(int_or_char_list3) + ":" + random.choice(int_or_char_list4) + random.choice(int_or_char_list4) + ":" + random.choice(int_or_char_list5) + random.choice(int_or_char_list5) + ":" + random.choice(int_or_char_list6) + random.choice(int_or_char_list6)
         print(address)
     
     # Only integer MAC address generation
@@ -33,8 +36,8 @@ def generator():
     else:
         address = ""
         for x in range(5):
-            address = address + random.choice(string.ascii_uppercase) + random.choice(string.ascii_uppercase) + ":"
-        address = address + random.choice(string.ascii_uppercase) + random.choice(string.ascii_uppercase)
+            address = address + random.choice(alphabet) + random.choice(alphabet) + ":"
+        address = address + random.choice(alphabet) + random.choice(alphabet)
         print(address)
 
 generator()
