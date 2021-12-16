c728db8e489b92f5c5172d6033158953d68a461b
Arnib Q <71690828+UchihaCryptic@users.noreply.github.com>
2021-12-15 16:11:42 -0800

    Added better compatibility for Windows, see README for proper usage

--- mac_addr_generator.py
+++ mac_addr_generator.py
@@ -1,8 +1,21 @@
 import random
 import string
+import platform
+import os
+
+def platformCheck(address):
+    if platform.system() == 'Windows':
+        f = open("mac_addr.txt", "w")
+        f.write("Here is your generated MAC address: " + address)
+        f.close()
+        os.startfile("mac_addr.txt")
+
 def generator():
     while True:
         try:
+            if platform.system() == 'Windows':
+                print("***You are using Windows, so expect to have a text file to open after choosing an option***")
+                print("\n")
             input_value = int(input("Choose a type of MAC Address:\n" +  "1) Default\n" + "2) Only Integer\n" + "3) Only Character\n"))
             if input_value > 0 and input_value < 4:
                 break
@@ -12,6 +25,7 @@ def generator():
         except:
             print("Choose '1', '2', or '3'")
     alphabet = ['A','B','C','D','E','F']
+    
     # Default MAC address Generation
     if input_value == 1:
         address = ""
@@ -23,6 +37,7 @@ def generator():
         int_or_char_list6 = [str(random.randint(0,9)), random.choice(alphabet)]
         address = random.choice(int_or_char_list1) + random.choice(int_or_char_list1) + ":" + random.choice(int_or_char_list2) + random.choice(int_or_char_list2) + ":" + random.choice(int_or_char_list3) + random.choice(int_or_char_list3) + ":" + random.choice(int_or_char_list4) + random.choice(int_or_char_list4) + ":" + random.choice(int_or_char_list5) + random.choice(int_or_char_list5) + ":" + random.choice(int_or_char_list6) + random.choice(int_or_char_list6)
         print(address)
+        platformCheck(address)
     
     # Only integer MAC address generation
     elif input_value == 2:
@@ -31,6 +46,7 @@ def generator():
             address = address + str(random.randint(0,9)) + str(random.randint(0,9)) + ":"
         address = address + str(random.randint(0,9)) + str(random.randint(0,9))
         print(address)
+        platformCheck(address)
     
     #Only character MAC address generation
     else:
@@ -39,5 +55,6 @@ def generator():
             address = address + random.choice(alphabet) + random.choice(alphabet) + ":"
         address = address + random.choice(alphabet) + random.choice(alphabet)
         print(address)
+        platformCheck(address)
 
 generator()
